ed9d39a01cbaf2122b381695b741f742f528606a
Joel Grant <joelmgrant@me.com>
2021-11-11 19:43:23 -0700

    Add initialize and adult?

---
+++ lib/passenger.rb
@@ -0,0 +1,13 @@
+class Passenger
+  attr_reader :name, :age
+
+  def initialize(details)
+    @name = details["name"]
+    @age = details["age"]
+  end
+
+  def adult?
+    @age >= 18
+  end
+  
+end
